1a45cb1a6d3e8bb94287d1991cc1b0d29df75b7a
Ndimande <mkatroso.deep@gmail.com>
2023-04-24 23:04:01 +0200

    IWA16

---
+++ Scripts.js
@@ -0,0 +1,90 @@
+const MONTHS = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+const data = {
+  response: {
+    requestType: "FETCH_ATHLETE_DATA",
+    requestBy: "ALL_MATCHING_ATHLETES",
+    forDisplay: "BEST_RACES",
+
+    data: {
+      NM372: {
+        firstName: "Nwabisa",
+        surname: "Masiko",
+        id: "NM372",
+        races: [
+          {
+            date: '2022-11-18T20:00:00.000Z',
+            time: [9, 7, 8, 6],
+          },
+          {
+            date: '2022-12-02T20:00:00.000Z',
+            time: [6, 7, 8, 7],
+          },
+        ],
+      },
+
+      SV782: {
+        firstName: "Schalk",
+        surname: "Venter",
+        id: "SV782",
+        races: [
+          {
+            date: '2022-11-18T20:00:00.000Z',
+            time: [10, 8, 3, 12],
+          },
+          {
+            date: '2022-11-25T20:00:00.000Z',
+            time: [6, 8, 9, 11],
+          },
+          {
+            date: '2022-12-02T20:00:00.000Z',
+            time: [10, 11, 4, 8],
+          },
+          {
+            date: '2022-12-09T20:00:00.000Z',
+            time: [9, 8, 9, 11],
+          },
+        ],
+      },
+    },
+  },
+};
+
+// Only edit below this comment
+
+const formatTime = (minutes) => {
+  const hours = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+  return `${hours.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}`;
+};
+
+const formatDate = (date) => {
+  const d = new Date(date);
+  const day = d.getDate().toString().padStart(2, '0');
+  const month = MONTHS[d.getMonth()];
+  const year = d.getFullYear();
+  return `${day} ${month} ${year}`;
+};
+
+const createHtml = (athleteData) => {
+  const { firstName, surname, id, races } = athleteData;
+  const [latestRace] = races.slice().reverse();
+  const { date, time } = latestRace;
+  const totalTime = formatTime(time.reduce((a, b) => a + b, 0));
+
+  const section = document.querySelector(`section[data-athlete="${id}"]`);
+  const heading = document.createElement('h2');
+  heading.textContent =
